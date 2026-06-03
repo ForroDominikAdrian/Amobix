b694e3242ef3eeabc6df27e4c205bbf3dab8dd2b
VargaAti <varga.attila@diak.szbi-pg.hu>
2026-06-03 08:21:37 +0200

    Update main.py

--- grafikus_felulet/main.py
+++ grafikus_felulet/main.py
@@ -14,7 +14,7 @@ oszlop = 3
 cella_meret = szelesseg // oszlop
 
 screen = pygame.display.set_mode((szelesseg, magassag))
-pygame.display.set_caption("Amőba + Pontszámok")
+pygame.display.set_caption("Amőba")
 
 # Betűtípus a pontszámokhoz
 font = pygame.font.SysFont("Arial", 30, bold=True)
